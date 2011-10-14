f2896883d165382a199c0237ba6f3684f669afa5
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2011-10-14 14:07:06 +0000

    fix pnotify and upgrade to 1.0.2
    git-svn-id: https://plugs.googlecode.com/svn/trunk@63 912515bb-fe1b-d127-1567-e02edd584596

--- plugs/ui/jquery/pnotify/template_plugins/pnotify.py
+++ plugs/ui/jquery/pnotify/template_plugins/pnotify.py
@@ -1,5 +1,5 @@
 def call(app, var, env):
     a = []
-    a.append('pnotify/jquery.pnotify.min.js')
     a.append('pnotify/jquery.pnotify.default.css')
+    a.append('pnotify/jquery.pnotify.min.js')
     return {'toplinks':a}

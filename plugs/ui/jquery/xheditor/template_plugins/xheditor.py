f2d59624a9b01e7c59b43b2aefdc368e4cbd9163
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2011-11-07 02:41:59 +0000

    upgrade xheditor to 1.1.11
    git-svn-id: https://plugs.googlecode.com/svn/trunk@78 912515bb-fe1b-d127-1567-e02edd584596

--- plugs/ui/jquery/xheditor/template_plugins/xheditor.py
+++ plugs/ui/jquery/xheditor/template_plugins/xheditor.py
@@ -1,4 +1,4 @@
-def call(app, var, env, ajaxForm=False):
+def call(app, var, env, ajaxForm=False, version='1.1.11'):
     a = []
-    a.append('xheditor/xheditor-1.1.9-zh-cn.min.js')
+    a.append('xheditor/xheditor-%s-zh-cn.min.js' % version)
     return {'toplinks':a, 'depends':[('jquery')]}

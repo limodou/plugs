1f7038649c5b8ccf941015861414fd16a13c9347
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2012-02-12 15:53:54 +0000

    upgrade bootstrap to 2.0 and fix jquery 1.7.1 to newest
    git-svn-id: https://plugs.googlecode.com/svn/trunk@103 912515bb-fe1b-d127-1567-e02edd584596

--- plugs/ui/bootstrap/template_plugins/bootstrap.py
+++ plugs/ui/bootstrap/template_plugins/bootstrap.py
@@ -1,18 +1,21 @@
-def call(app, var, env, plugins=None, use_less=False):
+def call(app, var, env, plugins=None, use_less=False, version=None):
+    from uliweb import settings
+    
     plugins = plugins or []
     a = []
+    version = version or settings.UI_CONFIG.bootstrap_version
     a.append('<!--[if lt IE 9]>')
-    a.append('bootstrap/asset/html5.js')
+    a.append('bootstrap/%s/asset/html5.js' % version)
     a.append('<![endif]-->')
     if use_less:
-        a.append('bootstrap/lib/bootstrap.less')
+        a.append('bootstrap/%s/lib/bootstrap.less' % version)
     else:
-        a.append('bootstrap/bootstrap.min.css')
+        a.append('bootstrap/%s/bootstrap.min.css' % version)
     
     jquery = False
     jquery_ui = False
     for x in plugins:
-        a.append('bootstrap/js/bootstrap-%s.js' % x)
+        a.append('bootstrap/%s/js/bootstrap-%s.js' % (version, x))
         jquery = True
         if x in ['pagination']:
             jquery_ui = True

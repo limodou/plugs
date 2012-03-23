74d23a7f7c52e6fbc1bfe6f0a872521f92cbf39c
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2012-03-23 09:31:37 +0000

    fix jquery
    git-svn-id: https://plugs.googlecode.com/svn/trunk@149 912515bb-fe1b-d127-1567-e02edd584596

--- plugs/ui/jquery/jquery/template_plugins/jquery.py
+++ plugs/ui/jquery/jquery/template_plugins/jquery.py
@@ -7,10 +7,10 @@ def call(app, var, env, version=None, ui=False, theme='redmond', css_only=False)
     if css_only:
         return {'toplinks':['jquery/ui/css/%s/jquery-ui-%s.custom.css' % (theme, ui_version)]}
     if version:
-        a.append('jquery/jquery-%s.js' % version)
+        a.append('jquery/jquery-%s.min.js' % version)
     if ui:
         a.append('jquery/ui/css/%s/jquery-ui-%s.custom.css' % (theme, ui_version))
-        a.append('jquery/jquery-%s.js' % version)
+        a.append('jquery/jquery-%s.min.js' % version)
         a.append('jquery/ui/js/jquery-ui-%s.custom.min.js' % ui_version)
         a.append('jquery/ui/js/jquery.ui.datepicker.zh.js')
         

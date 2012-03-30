63c6091719a8ee6d0c0333defd37b89c42dd848d
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2012-03-30 10:17:30 +0000

    fix
    git-svn-id: https://plugs.googlecode.com/svn/trunk@160 912515bb-fe1b-d127-1567-e02edd584596

--- plugs/ui/jquery/ztree/template_plugins/ztree.py
+++ plugs/ui/jquery/ztree/template_plugins/ztree.py
@@ -1,9 +1,9 @@
-def call(app, var, env, plugins=None):
+def call(app, var, env, plugins=None, version=None, all=True):
     from uliweb import settings
     
     plugins = plugins or []
     a = []
-    version = settings.UI_CONFIG.ztree_version
+    version = version or settings.UI_CONFIG.ztree_version
     if version == '2.6':
         a.append('ztree/%s/style/zTreeStyle.css' % version)
         a.append('ztree/%s/style/zTreeIcons.css' % version)
@@ -13,5 +13,13 @@ def call(app, var, env, plugins=None):
         a.append('ztree/%s/jquery.ztree.core-%s.min.js' % (version, version))
         for p in plugins:
             a.append('ztree/%s/jquery.ztree.%s-%s.min.js' % (version, p, version))
+    elif version == '3.1':
+        a.append('ztree/%s/css/zTreeStyle.css' % version)
+        if all:
+            a.append('ztree/%s/jquery.ztree.all-3.1.min.js' % version)
+        else:
+            a.append('ztree/%s/jquery.ztree.core-%s.min.js' % (version, version))
+            for p in plugins:
+                a.append('ztree/%s/jquery.ztree.%s-%s.min.js' % (version, p, version))
         
     return {'toplinks':a, 'depends':['jquery']}

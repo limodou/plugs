1e24c94f51e319cb5830b6150cb1bb6dea61d19e
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2012-04-08 13:46:35 +0000

    fix get_user_image_url bug(for user notfound)
    git-svn-id: https://plugs.googlecode.com/svn/trunk@170 912515bb-fe1b-d127-1567-e02edd584596

--- plugs/user_admin/__init__.py
+++ plugs/user_admin/__init__.py
@@ -7,13 +7,15 @@ def _get_portrait_image_thumbnail(id, size=50):
     return os.path.join('portraits', str(id) + '.%dx%d' % (size, size) + '.jpg')
 
 def get_user_image(user, size=50):
+    from uliweb.contrib.staticfiles import url_for_static
     from uliweb import functions
     import os
     
-    image = functions.get_filename(_get_portrait_image_thumbnail(user.id, size))
-    if os.path.exists(image):
-        image_url = functions.get_href(_get_portrait_image_thumbnail(user.id, size))
-    else:
-        image_url = user.get_default_image_url(size)
-    return image_url
+    if user:
+        image = functions.get_filename(_get_portrait_image_thumbnail(user.id, size))
+        if os.path.exists(image):
+            image_url = functions.get_href(_get_portrait_image_thumbnail(user.id, size))
+            return image_url
+
+    return functions.url_for_static('images/user%dx%d.jpg' % (size, size))
     

7091832bc52c5e78d524ad2ff87c4ff52c325d65
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2012-04-13 08:08:35 +0000

    add js and fix bootstrap
    git-svn-id: https://plugs.googlecode.com/svn/trunk@181 912515bb-fe1b-d127-1567-e02edd584596

--- plugs/ui/jquery/jqutils/template_plugins/functional.py
+++
@@ -1,4 +0,0 @@
-def call(app, var, env, ajaxForm=False):
-    a = []
-    a.append('jqutils/functional.min.js')
-    return {'toplinks':a}
--- plugs/ui/js/jsutils/template_plugins/less.py
+++ plugs/ui/js/jsutils/template_plugins/less.py
@@ -3,5 +3,5 @@ def call(app, var, env, version=None):
     
     version = version or settings.UI_CONFIG.less_version
     a = []
-    a.append('jqutils/less-%s.min.js' % version)
+    a.append('jsutils/less-%s.min.js' % version)
     return {'bottomlinks':a}

f6df45df824d0cea3a06a8dccd1c133e8d85461b
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2012-05-17 14:44:01 +0000

    change jquery.form setting in jqutils
    git-svn-id: https://plugs.googlecode.com/svn/trunk@204 912515bb-fe1b-d127-1567-e02edd584596

--- plugs/ui/jquery/jqutils/template_plugins/jqutils.py
+++ plugs/ui/jquery/jqutils/template_plugins/jqutils.py
@@ -1,12 +1,13 @@
 def call(app, var, env, ajaxForm=False, hoverIntent=False, spin=False):
     a = []
+    depends = [('jquery', {'ui':True})]
     a.append('jqutils/jqutils.css')
     if spin:
         a.append('jqutils/spin.min.js')
     a.append('jqutils/jqrselect.js')
     a.append('jqutils/jqutils.js')
     if ajaxForm:
-        a.append('jqutils/jquery.form.js')
+        depends.append('jqform')
     if hoverIntent:
         a.append('jqutils/jquery.hoverIntent.minified.js')
-    return {'toplinks':a, 'depends':[('jquery', {'ui':True})]}
+    return {'toplinks':a, 'depends':depends}

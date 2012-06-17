5d7b747701d13add88917c6563eddfa5afeeaf3a
mzehrer <zehrer@zepan.org>
2012-06-17 14:03:17 +0200

    Added support for first and last page

--- plugs/ui/bootstrap/static/bootstrap/2.0.3/js/bootstrap-pagination.js
+++ plugs/ui/bootstrap/static/bootstrap/2.0.3/js/bootstrap-pagination.js
@@ -17,161 +17,183 @@
  * ========================================================= */
 
 
-!function( $ ){
+!
+function($) {
 
   "use strict"
 
   /* MODAL CLASS DEFINITION
    * ====================== */
 
-  var Pagination = function ( content, options ) {
-    var that = this;
-    this.options = options;
-    this.$element = $(content).addClass('pagination').empty();
-    var list = $('<ul/>').appendTo(this.$element);
-    if (this.options.totalMessage) list.append($('<li class="disabled"><a href="#"></a></li>'));
-    var btnPrev = this.btnPrev = $('<li class="prev"><a href="#">' + this.options.prev + '</a></li>');
-    var btnNext = this.btnNext = $('<li class="next"><a href="#">'+this.options.next+'</a></li>');
-    list.append(btnPrev).append(btnNext);
-    this.$element.delegate('li', 'click.pagination', function(e){
+  var Pagination = function(content, options) {
+
+      var that = this;
+      this.options = options;
+
+      this.$element = $(content).addClass('pagination').empty();
+      var list = $('<ul/>').appendTo(this.$element);
+
+      var btnFirst = this.btnFirst = $('<li class="first"><a href="#">' + this.options.first + '</a></li>');
+      var btnPrev = this.btnPrev = $('<li class="prev"><a href="#">' + this.options.prev + '</a></li>');
+      var btnNext = this.btnNext = $('<li class="next"><a href="#">' + this.options.next + '</a></li>');
+      var btnLast = this.btnLast = $('<li class="last"><a href="#">' + this.options.last + '</a></li>');
+
+      list.append(btnPrev).append(btnNext);
+      list.append(this.btnLast);
+      list.prepend(this.btnFirst);
+
+      this.$element.delegate('li:enabled, li', 'click.pagination', function(e) {
+      
         e.preventDefault();
+      
         var $this = $(this);
-        if ($this.hasClass('disabled')) return;
         var list = that.$element.find('ul');
-        if ($this.hasClass('prev')){
-            that.currentPage = parseInt(list.find('li.active a').text()) - 1; 
-            
-        }else if($this.hasClass('next')){
-            that.currentPage = parseInt(list.find('li.active a').text()) + 1; 
-        }else{
-            that.currentPage = parseInt($this.text());
+      
+        if ($this.hasClass('prev')) {
+          that.currentPage = parseInt(list.find('li.active a').text()) - 1;
+        } else if ($this.hasClass('next')) {
+          that.currentPage = parseInt(list.find('li.active a').text()) + 1;
+        } else if ($this.hasClass('first')) {
+          that.currentPage = 1;
+        } else if ($this.hasClass('last')) {
+          that.currentPage = (that.options.total);
+        } else {
+          that.currentPage = parseInt($this.text());
         }
-        that.load(--that.currentPage); 
-    });
-  }
+      
+        if (!$this.hasClass('disabled')) {
+          that.load(--that.currentPage);
+        }
+      
+      });
+
+    }
 
   Pagination.prototype = {
-  
-      constructor: Pagination
-    , show: function(total, start){
-        this.options.start = start || this.options.start;
-        this.options.total = total || this.options.total;
-        this.currentPage = this.options.start - 1;
-        this.totalPages = parseInt(this.options.total / this.options.pageRows);
-        if (this.options.total % this.options.pageRows > 0) this.totalPages++;
-        if (this.options.totalMessage){
-            var msg = this.options.totalMessage.replace('$pages', this.totalPages);
-            msg = msg.replace('$records', this.options.total);
-            this.$element.find('ul > li:first > a').text(msg);
-        }
-        navigate.call(this, this.currentPage);
-        if (this.options.initLoad){
-            this.load(this.currentPage);
-        }
+
+    constructor: Pagination,
+    show: function(total, start) {
+      this.options.start = start || this.options.start;
+      this.options.total = total || this.options.total;
+      this.currentPage = this.options.start - 1;
+      this.totalPages = parseInt(this.options.total / this.options.pageRows);
+      if (this.options.total % this.options.pageRows > 0) this.totalPages++;
+      navigate.call(this, this.currentPage);
+      if (this.options.initLoad) {
+        this.load(this.currentPage);
       }
-    , load: function (page){
-        navigate.call(this, page);
-        if (this.options.onChange) {
-            this.options.onChange((this.currentPage + 1));
-        }
+    },
+    load: function(page) {
+      navigate.call(this, page);
+      if (this.options.onChange) {
+        this.options.onChange((this.currentPage + 1));
+      }
     }
   }
 
- /* MODAL PRIVATE METHODS
-  * ===================== */
+  /* MODAL PRIVATE METHODS
+   * ===================== */
 
   function _buildNavigation(startPage) {
-      var s = this.options;
-      var self = this;
-      var list = this.$element.find('ul');
-//      if (s.total <= s.pageRows) return;
-      var target = list.find('li:last');
-      for (var i = startPage; i < startPage + s.length; i++) {
-          if (i == this.totalPages) break;
-          var li = $('<li/>')
-              .insertBefore(target)
-              .append($('<a>').attr('rel', (i + 1))
-              .attr('href', '#')
-              .text(i + 1));
-      }
+
+    var s = this.options;
+    var self = this;
+    var list = this.$element.find('ul');
+
+    if (s.total <= s.pageRows) return;
+
+    var target = list.find('li:last').prev();
+
+    for (var i = startPage; i < startPage + s.length; i++) {
+      if (i == this.totalPages) break;
+      var li = $('<li/>').insertBefore(target).append($('<a>').attr('rel', (i + 1)).attr('href', '#').text(i + 1));
+    }
+
+    this.$element.find('ul').append(this.btnLast);
+    this.$element.find('ul').prepend(this.btnFirst);
+
   }
 
   function navigate(topage) {
-      var s = this.options;
-      var list = this.$element.find('ul');
-      var start = 0;
-      if(this.options.totalMessage) start = 1;
-      for(var i=list.find('li').size()-2; i>start; i--){
-          list.find('li').eq(i).remove();
+
+    var s = this.options;
+    var list = this.$element.find('ul');
+    for (var i = list.find('li').size() - 4; i > 0; i--) {
+      list.find('li').eq(i+1).remove();
+    }
+    var index = topage;
+    var mid = s.length / 2;
+    if (s.length % 2 > 0) mid = (s.length + 1) / 2;
+    var startIndex = 0;
+    if (topage >= 0 && topage < this.totalPages) {
+      if (topage >= mid) {
+        if (this.totalPages - topage > mid) startIndex = topage - (mid - 1);
+        else if (this.totalPages > s.length) startIndex = this.totalPages - s.length;
       }
-      var index = topage;
-      var mid = s.length / 2;
-      if (s.length % 2 > 0) mid = (s.length + 1) / 2;
-      var startIndex = 0;
-      if (topage >= 0 && topage < this.totalPages) {
-          if (topage >= mid) {
-              if (this.totalPages - topage > mid)
-                  startIndex = topage - (mid - 1);
-              else if (this.totalPages > s.length)
-                  startIndex = this.totalPages - s.length;
-          }
-          _buildNavigation.call(this, startIndex); 
-          list.find('li').removeClass('active');
-          list.find('li a[rel='+(index+1)+']').parent().addClass('active');
-      }
-      _showRequiredButtons.call(this);
+      _buildNavigation.call(this, startIndex);
+      list.find('li').removeClass('active');
+      list.find('li a[rel=' + (index + 1) + ']').parent().addClass('active');
+    }
+    
+    _showRequiredButtons.call(this);
+
   }
 
   function _showRequiredButtons() {
-      var s = this.options;
-      if (this.totalPages > 1) {
-          if (this.currentPage > 0) 
-              this.btnPrev.removeClass('disabled'); 
-          else 
-              this.btnPrev.addClass('disabled'); 
-  
-          if (this.currentPage == this.totalPages - 1)
-              this.btnNext.addClass('disabled'); 
-          else 
-              this.btnNext.removeClass('disabled');
+    var s = this.options;
+    if (this.totalPages > 1) {
+      if (this.currentPage > 0) {
+        this.btnPrev.removeClass('disabled');
+        this.btnFirst.removeClass('disabled');
+      } else {
+        this.btnPrev.addClass('disabled');
+        this.btnFirst.addClass('disabled');
       }
-      else {
-          this.btnPrev.addClass('disabled');
-          this.btnNext.addClass('disabled');
+
+      if (this.currentPage == this.totalPages - 1) {
+        this.btnNext.addClass('disabled');
+        this.btnLast.addClass('disabled');
+      } else {
+        this.btnNext.removeClass('disabled');
+        this.btnLast.removeClass('disabled');
       }
+    } else {
+      this.btnPrev.addClass('disabled');
+      this.btnNext.addClass('disabled');
+      this.btnFirst.addClass('disabled');
+      this.btnLast.addClass('disabled');
+    }
   }
-  
 
- /* MODAL PLUGIN DEFINITION
-  * ======================= */
 
-  $.fn.pagination = function (option) {
+  /* MODAL PLUGIN DEFINITION
+   * ======================= */
+
+  $.fn.pagination = function(option) {
     var args = Array.prototype.slice.call(arguments);
     args.shift();
-    return this.each(function () {
-      var $this = $(this)
-        , data = $this.data('pagination')
-        , options = $.extend({}, $.fn.pagination.defaults, typeof option == 'object' && option);
+    return this.each(function() {
+      var $this = $(this),
+        data = $this.data('pagination'),
+        options = $.extend({}, $.fn.pagination.defaults, typeof option == 'object' && option);
       if (!data) $this.data('pagination', (data = new Pagination(this, options)));
       if (typeof option == 'string') data[option].apply(data, args);
     })
   }
 
   $.fn.pagination.defaults = {
-      total: 0,
-      pageRows: 0,
-      length: 10,
-      next: 'Next',
-      prev: 'Prev',
-      first: 'First',
-      last: 'Last',
-      start: 1,
-      totalMessage: 'Total $pages pages / $records records', //if not then doesn't display at all
-      initLoad: false,
-      onChange: null
+    total: 0,
+    pageRows: 0,
+    length: 10,
+    next: 'Next',
+    prev: 'Prev',
+    first: 'First',
+    last: 'Last',
+    start: 1,
+    initLoad: false,
+    onChange: null
   }
 
   $.fn.pagination.Constructor = Pagination;
 
-}( window.jQuery );
-
+}(window.jQuery);

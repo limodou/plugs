249f2e647120210968f69e0a4bb08d4a4c6161fc
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2012-10-12 14:20:10 +0000

    improve rbac_man
    git-svn-id: https://plugs.googlecode.com/svn/trunk@271 912515bb-fe1b-d127-1567-e02edd584596

--- plugs/rbac_man/views.py
+++ plugs/rbac_man/views.py
@@ -4,14 +4,19 @@ from uliweb.orm import get_model
 from uliweb.i18n import ugettext_lazy as _
 
 def __begin__():
-    from uliweb import function
-    return function('has_login')()
+    from uliweb import functions
+    return functions.require_login()
     
-@expose('/config/roles')
+@expose('/admin/roles')
 class RoleView(object):
     def __init__(self):
         self.model = get_model('role')
         
+    def __begin__(self):
+        if not request.user.is_superuser:
+            error('你不是超级用户不能进行这项操作！')
+        
+    @expose('')
     def list(self):
         from uliweb.utils.generic import ListView
         
@@ -22,7 +27,7 @@ class RoleView(object):
             return '<a href="%s">%s</a>' % (url_for(RoleView.view, id=obj.id), value)
             
         fields = [
-            {'name':'name', 'verbose_name':_('Name'), 'width':80},
+            {'name':'name', 'verbose_name':_('Name'), 'width':150},
             {'name':'description', 'verbose_name':_('Description'), 'width':200},
             {'name':'reserve', 'verbose_name':_('Is Reserved'), 'width':80},
         ]
@@ -105,6 +110,37 @@ class RoleView(object):
         view = DeleteView(self.model, url_for(RoleView.list), obj=obj)
         return view.run()
     
+    def addbatchuser(self):
+        User = get_model('user')
+        Role = get_model('role')
+        
+        user_ids = request.POST.get('user_ids')
+        role_id = request.POST.get('role_id')
+        
+        user_ids = user_ids.replace('\n', ',').replace('\r', '').replace(u'，',',')
+        user_ids = user_ids.split(',')
+        
+        error_users = []
+        print user_ids
+        for user_id in user_ids:
+            if user_id != '':
+                user = User.get(User.c.username == user_id)
+                if not user:
+                    error_users.append(user_id)
+
+        if len(error_users)>0:
+            return json({'success':False, 'message': u"下列人员帐号找不到： %s， 请仔细检查后再次增加。" % ','.join(error_users)})  
+        else:
+            role = Role.get(int(role_id))
+            for user_id in user_ids:
+                user = User.get(User.c.username == user_id)
+                if not role.users.has(user):
+                    role.users.add(user)
+            message = "批量增加人员成功"
+            flash(message, category='success')
+            return json({'success':True, 'message': message});
+
+    
     def adduser(self):
         User = get_model('user')
         Role = get_model('role')
@@ -121,7 +157,10 @@ class RoleView(object):
             return json({'success':False, 'message':"The user %s has already existed in role %s" % (user.username, role.name)})
         else:
             role.users.add(user)
-            return json({'success':True, 'data':{'username':user.username, 'id':user.id}, 'message':"The user %s added to role %s successfully" % (user.username, role.name)})
+            userdata = {'username':user.username, 'id':user.id}
+            if hasattr(user, 'nickname'):
+                userdata['nickname'] = user.nickname
+            return json({'success':True, 'data':userdata, 'message':"The user %s added to role %s successfully" % (user.username, role.name)})
             
     def deluser(self):
         User = get_model('user')
@@ -177,11 +216,24 @@ class RoleView(object):
         else:
             return json({'success':False, 'message':"The permission %s is not existed in role %s successfully." % (perm.name, role.name)})
     
-@expose('/config/permissions')
+    def search(self):
+        name = request.GET.get('term', '')
+        if name:
+            result = [{'id':obj.id, 'text':obj.name} for obj in self.model.filter(self.model.c.name.like(name + '%%'))]
+        else:
+            result = []
+        return json(result)
+    
+@expose('/admin/permissions')
 class PermissionView(object):
     def __init__(self):
         self.model = get_model('permission')
         
+    def __begin__(self):
+        if not request.user.is_superuser:
+            error('你不是超级用户不能进行这项操作！')
+    
+    @expose('')
     def list(self):
         from uliweb.utils.generic import ListView
         
@@ -192,7 +244,7 @@ class PermissionView(object):
             return '<a href="%s">%s</a>' % (url_for(PermissionView.view, id=obj.id), value)
             
         fields = [
-            {'name':'name', 'verbose_name':_('Name'), 'width':80},
+            {'name':'name', 'verbose_name':_('Name'), 'width':200},
             {'name':'description', 'verbose_name':_('Description'), 'width':200},
         ]
         fields_convert_map = {'name':name}
@@ -224,7 +276,7 @@ class PermissionView(object):
         
     def add(self):
         """
-        Add new role
+        Add new permission
         """
         
         from uliweb.utils.generic import AddView
@@ -238,7 +290,7 @@ class PermissionView(object):
             {'name':'name', 'verbose_name':_('Name')},
             {'name':'description', 'verbose_name':_('Description')},
         ]
-
+        
         view = AddView(self.model, url, default_data=default_data, fields=fields)
         return view.run()
     
@@ -284,8 +336,45 @@ class PermissionView(object):
     def search(self):
         name = request.GET.get('term', '')
         if name:
-            result = [{'id':obj.id, 'title':obj.name} for obj in self.model.filter(self.model.c.name.like(name + '%%'))]
+            result = [{'id':obj.id, 'text':obj.name} for obj in self.model.filter(self.model.c.name.like(name + '%%'))]
         else:
             result = []
         return json(result)
+    
+
+    def addrole(self):
+        Perm = get_model('permission')
+        Role = get_model('role')
+        
+        perm_id = request.POST.get('perm_id')
+        role_id = request.POST.get('role_id')
+        perm = Perm.get(int(perm_id))
+        role = Role.get(int(role_id))
+        if not perm:
+            return json({'success':False, 'message':"Can't find the permission id %s" % perm_id})
+        if not role:
+            return json({'success':False, 'message':"Can't find the role id %s" % role_id})
+        if perm.perm_roles.has(role):
+            return json({'success':False, 'message':"The role %s has already existed in permission %s" % (role.name, perm.name)})
+        else:
+            perm.perm_roles.add(role)
+            return json({'success':True, 'data':{'name':role.name, 'id':role.id}, 'message':"The role %s added to permission %s successfully" % (role.name, perm.name)})
+            
+    def delrole(self):
+        Perm = get_model('permission')
+        Role = get_model('role')
+        
+        perm_id = request.POST.get('perm_id')
+        role_id = request.POST.get('role_id')
+        perm = Perm.get(int(perm_id))
+        role = Role.get(int(role_id))
+        if not perm:
+            return json({'success':False, 'message':"Can't find the permission id %s" % perm_id})
+        if not role:
+            return json({'success':False, 'message':"Can't find the role id %s" % role_id})
+        if perm.perm_roles.has(role):
+            perm.perm_roles.remove(role)
+            return json({'success':True, 'message':"The permission %s has been delete from role %s successfully." % (perm.name, role.name)})
+        else:
+            return json({'success':False, 'message':"The permission %s is not existed in role %s successfully." % (perm.name, role.name)})
     

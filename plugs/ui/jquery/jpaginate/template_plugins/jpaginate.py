e94178bf44e609a87af5c7c0cefce41cd9b76b76
zhangclb <zhangclb@lenovo.com>
2014-11-20 16:23:04 +0800

    fix jpaginate missing image issue,in style.css use ../images/previous.png, so put style.css in css directory

--- plugs/ui/jquery/jpaginate/template_plugins/jpaginate.py
+++ plugs/ui/jquery/jpaginate/template_plugins/jpaginate.py
@@ -1,5 +1,5 @@
 def call(ajaxForm=False):
     a = []
-    a.append('jpaginate/style.css')
+    a.append('jpaginate/css/style.css')
     a.append('jpaginate/jquery.paginate.js')
     return {'toplinks':a, 'depends':[('jquery')]}

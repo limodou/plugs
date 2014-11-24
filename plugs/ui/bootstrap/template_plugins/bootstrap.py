82d072197e0d155dbb290a3c06d1fe2fa6689db2
zhengwt1 <zhengwt1@lenovo.com>
2014-11-24 11:26:48 +0800

    解决bootstrap 2.3.2无法使用Glyphicons图标的问题
    css目录结构保持与getbootstrap.com下载的一致。

--- plugs/ui/bootstrap/template_plugins/bootstrap.py
+++ plugs/ui/bootstrap/template_plugins/bootstrap.py
@@ -1,20 +1,25 @@
 def call(plugins=None, version=None):
     from uliweb import settings
-    
+
     plugins = plugins or []
     a = []
     version = version or settings.UI_CONFIG.bootstrap_version
     a.append('<!--[if lt IE 9]>')
     a.append('bootstrap/asset/html5.js')
     a.append('<![endif]-->')
-    a.append('bootstrap/%s/bootstrap.min.css' % version)
-#    a.append('bootstrap/%s/bootstrap-responsive.min.css' % version)
+    _v = map(int, version.split('.'))
+    if _v > [2, 3]:
+        a.append('bootstrap/%s/css/bootstrap.min.css' % version)
+#       a.append('bootstrap/%s/css/bootstrap-responsive.min.css' % version)
+    else:
+        a.append('bootstrap/%s/bootstrap.min.css' % version)
+#       a.append('bootstrap/%s/bootstrap-responsive.min.css' % version)
 
     for x in plugins:
         a.append('bootstrap/%s/js/bootstrap-%s.js' % (version, x))
 
     a.append('bootstrap/%s/js/bootstrap.min.js' % version)
-      
+
     d = {'toplinks':a, 'depends':[]}
     d['depends'] = ['jquery', 'json2']
     return d
